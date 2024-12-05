3090a3404314595f9d6a4b95e53ca7c51c38b62d
rebecaaras <rebecaaraujo2013@gmail.com>
2024-12-05 19:30:10 -0300

    Place content inside Page component

--- index.jsx
+++ index.jsx
@@ -1,30 +1,35 @@
 import "./index.css"
 import { createRoot } from "react-dom/client";
+
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <body>
-    <main>
-      <img src="react-logo.svg" width="40px"/>
-      <h1>Fun facts about React:</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 200K stars on GitHub</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-      <Page/>
-    </main>
-    <img class="react-background"src="react-logo.svg" width="400px"/>
-  </body>
+  <Page/>
 )
 
 function Page(){
   return (
-    <ol>
-      <li>React is a popular library</li>
-      <li>Many job openings ask for React</li>
-    </ol>
+    <>
+      <header>
+        <img src="react-logo.svg" width="40px" alt="react logo"/>
+      </header>
+      <main>
+        <h1>Fun facts about React:</h1>
+        <ol>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 200K stars on GitHub</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ol>
+      </main>
+      <footer>
+        <small>© 2024 Aras development. All rights reserved.</small>
+      </footer>
+        
+    </>
+    
   )
 }
+
+{/* <img class="react-background"src="react-logo.svg" width="400px"/> */}
