47208423115fe49aeccd415dfb9a05c3427c3af1
rebecaaras <rebecaaraujo2013@gmail.com>
2024-12-06 12:20:10 -0300

    Move main content, hearder and footer to its own components

--- index.jsx
+++ index.jsx
@@ -3,33 +3,49 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(
-  <Page/>
-)
+function Header(){
+  return(
+    <header>
+        <img src="react-logo.svg" width="40px" alt="react logo"/>
+      </header>
+  )
+}
+
+function MainContent(){
+  return(
+    <main>
+      <h1>Fun facts about React:</h1>
+      <ol>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ol>
+    </main>
+  )
+}
+
+function Footer(){
+  return(
+    <footer>
+      <small>© 2024 Aras development. All rights reserved.</small>
+    </footer>
+  )
+}
 
 function Page(){
   return (
     <>
-      <header>
-        <img src="react-logo.svg" width="40px" alt="react logo"/>
-      </header>
-      <main>
-        <h1>Fun facts about React:</h1>
-        <ol>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 200K stars on GitHub</li>
-          <li>Is maintained by Meta</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ol>
-      </main>
-      <footer>
-        <small>© 2024 Aras development. All rights reserved.</small>
-      </footer>
-        
+      <Header/>
+      <MainContent/>
+      <Footer/>        
     </>
-    
   )
 }
+
+root.render(
+  <Page/>
+)
 
 {/* <img class="react-background"src="react-logo.svg" width="400px"/> */}
